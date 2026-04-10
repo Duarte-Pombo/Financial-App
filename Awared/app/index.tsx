ecb122e043e7645e72eac427c0202da54414ad2b
Camila <up202305458@up.pt>
2026-04-10 17:08:13 +0100

    improved login page

--- Awared/app/index.tsx
+++ Awared/app/index.tsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, View, StyleSheet, Pressable, TextInput } from "react-native";
+import { navigate } from "expo-router/build/global-state/routing";
 
 export default function Login() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Login Page</Text>
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontSize: 20, marginBottom: 25 }}>Welcome Back!</Text>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Password" />
+        <Pressable style={styles.button} onPress={attemptLogin}>
+          <Text style={{ textAlign: "center" }}>Login</Text>
+        </Pressable>
+
+      </View>
+      <View style={{ flex: 1 / 4, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Text>New here?</Text>
+        <Pressable style={styles.button} onPress={attemptLogin}>
+          <Text style={{ textAlign: "center" }}>Register</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+function attemptLogin() {
+  navigate("/(tabs)");
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    width: "80%",
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+  },
+  button: {
+    height: 40,
+    width: "40%",
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    backgroundColor: "pink",
+  }
+});
