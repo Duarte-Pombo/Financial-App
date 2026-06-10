876e1386514872f7a830bd09b4fe175545c0f1ed
Camila <up202305458@up.pt>
2026-06-10 18:28:34 +0100

    added server template

--- Awared/server-notifs/main.py
+++ Awared/server-notifs/main.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/savePurchase', methods=['GET', 'POST'])
+def savePurchase():
+    if request.method == 'POST':
+        user = request.form['user']
+        token = request.form['token']
+        time = request.form['time']
+        print(f"{user} {token} {time}")
+    return ""
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
